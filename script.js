3d031d64c321d5d8f21379324ba428ead0e60faa
JawalArcilla <>
2021-05-17 16:35:46 +0000

    Attempt #8 on fixing errors

--- script.js
+++ script.js
@@ -103,8 +103,8 @@ function clearScreen () {
 function drawSnake () {
   ctx.fillStyle = 'green'
   for (let i = 0; i < snakeParts.length; i++) {
-      const part = snakeParts[i]
-      ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+        const part = snakeParts[i]
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
   }
 
   snakeParts.push(new SnakePart(headX, headY))
@@ -137,37 +137,37 @@ function checkAppleCollision () {
 
 document.body.addEventListener('keydown', keyDown)
 
-function keyDown(event) {
+function keyDown (event) {
 //  up arrow
   if (event.keyCode === 38) {
     if (yVelocity === 1)
-    return
-      yVelocity = -1
-      xVelocity = 0
+  return
+        yVelocity = -1
+        xVelocity = 0
   }
 
 //  down arrow
   if (event.keyCode === 40) {
     if (yVelocity === -1)
-    return
-      yVelocity = 1
-      xVelocity = 0
+  return
+        yVelocity = 1
+        xVelocity = 0
   }
 
 //  left arrow
   if (event.keyCode === 37) {
     if (xVelocity === 1)
-    return
-      yVelocity = 0
-      xVelocity = -1
+  return
+        yVelocity = 0
+        xVelocity = -1
   }
 
 //  right arrow
   if (event.keyCode === 39) {
     if (xVelocity === -1)
     return
-    yVelocity = 0
-    xVelocity = 1
+         yVelocity = 0
+         xVelocity = 1
   }
 }
 
