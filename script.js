141faf77a8d71acf68a12c7c0cd27b11a704ebda
JawalArcilla <>
2021-05-17 17:36:01 +0000

    Attempt #12B

--- script.js
+++ script.js
@@ -138,41 +138,33 @@ function checkAppleCollision () {
 document.body.addEventListener('keydown', keyDown)
 
 function keyDown (event) {
-  // up arrow
-  if (event.keyCode === 38) {
-    if (yVelocity === 1) {
-    return
-    yVelocity = -1
-    xVelocity = 0
+  // up
+  if (event.keyCode == 38) {
+    if (yVelocity == 1) return;
+    yVelocity = -1;
+    xVelocity = 0;
   }
 
-  // down arrow
-  if (event.keyCode === 40) {
-    if (yVelocity === -1) {
-    return
-    yVelocity = 1
-    xVelocity = 0
+  // down
+  if (event.keyCode == 40) {
+    if (yVelocity == -1) return;
+    yVelocity = 1;
+    xVelocity = 0;
   }
 
-  // left arrow
-  if (event.keyCode === 37) {
-    if (xVelocity === 1) {
-    return
-    yVelocity = 0
-    xVelocity = -1
+  // left
+  if (event.keyCode == 37) {
+    if (xVelocity == 1) return;
+    yVelocity = 0;
+    xVelocity = -1;
   }
 
-  // right arrow
-  if (event.keyCode === 39) {
-    if (xVelocity === -1) {
-    return
-    yVelocity = 0
-    xVelocity = 1
-    }
-  }
-  }
-  }
+  // right
+  if (event.keyCode == 39) {
+    if (xVelocity == -1) return;
+    yVelocity = 0;
+    xVelocity = 1;
   }
 }
-a
+
 drawGame()
