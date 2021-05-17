ec01e71c8e53c0755301c541b25084d447bf91fc
JawalArcilla <>
2021-05-17 17:20:31 +0000

    Attempt #11A

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ document.body.addEventListener('keydown', keyDown)
 function keyDown (event) {
   // up arrow
   if (event.keyCode === 38) {
-    if (yVelocity === 1) {}
+    if (yVelocity === 1) 
       return
     yVelocity = -1
     xVelocity = 0
@@ -148,7 +148,7 @@ function keyDown (event) {
 
   // down arrow
   if (event.keyCode === 40) {
-    if (yVelocity === -1) {}
+    if (yVelocity === -1) 
       return
     yVelocity = 1
     xVelocity = 0
@@ -156,7 +156,7 @@ function keyDown (event) {
 
   // left arrow
   if (event.keyCode === 37) {
-    if (xVelocity === 1) {}
+    if (xVelocity === 1) 
       return
     yVelocity = 0
     xVelocity = -1
@@ -164,7 +164,7 @@ function keyDown (event) {
 
   // right arrow
   if (event.keyCode === 39) {
-    if (xVelocity === -1) {}
+    if (xVelocity === -1) 
       return
     yVelocity = 0
     xVelocity = 1
