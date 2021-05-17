bb8691160c406827349333665ed134f3e8219cb0
JawalArcilla <>
2021-05-17 17:21:07 +0000

    Attempt #11B

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ function keyDown (event) {
   // up arrow
   if (event.keyCode === 38) {
     if (yVelocity === 1) 
-      return
+    return
     yVelocity = -1
     xVelocity = 0
   }
@@ -149,7 +149,7 @@ function keyDown (event) {
   // down arrow
   if (event.keyCode === 40) {
     if (yVelocity === -1) 
-      return
+    return
     yVelocity = 1
     xVelocity = 0
   }
@@ -157,7 +157,7 @@ function keyDown (event) {
   // left arrow
   if (event.keyCode === 37) {
     if (xVelocity === 1) 
-      return
+    return
     yVelocity = 0
     xVelocity = -1
   }
@@ -165,7 +165,7 @@ function keyDown (event) {
   // right arrow
   if (event.keyCode === 39) {
     if (xVelocity === -1) 
-      return
+    return
     yVelocity = 0
     xVelocity = 1
   }
